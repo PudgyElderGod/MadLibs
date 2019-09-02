5b33d38a1616462742847fdc3a7982019ebfdcd5
PudgyElderGod <PudgyElderGod@gmail.com>
2019-09-01 22:51:56 -0700

    Finish story.

--- Madlibs.py
+++ Madlibs.py
@@ -1,12 +1,17 @@
 
 adj1 = input("Enter an adjective: ")
 TownName = input("Enter a proper noun: ")
-CreatureName = input("Enter an animal:")
-Verb1 = input("Enter a verb:")
+CreatureName = input("Enter an animal: ")
+Verb1 = input("Enter a verb: ")
 PlurN1 = input("Enter a plural noun: ")
 OrgName = input("Enter a proper noun: ")
+Years = input("Enter a number: ")
+adj2 = input("Enter an adjective: ")
+Noun = input("Enter a noun: ")
 
 
 print("In the", adj1, "town of", TownName, ", trouble is brewing.")
 print("Roaming packs of ", CreatureName, Verb1, " through the streets.")
 print("Luckily for the ", PlurN1, "the ", OrgName, " squad is on the hunt."  )
+print("After ", Years, " years they finally came up with an effective weapon against the ", CreatureName)
+print(adj2," ", Noun, ".")
