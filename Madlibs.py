593f0e736404211e6eb08dbb92c4028f1e42f420
PudgyElderGod <PudgyElderGod@gmail.com>
2019-09-01 21:54:25 -0700

    Restructured story; Wrote more

--- Madlibs.py
+++ Madlibs.py
@@ -1,6 +1,12 @@
 
 adj1 = input("Enter an adjective: ")
-TownName = input("Enter a name for a town: ")
+TownName = input("Enter a proper noun: ")
+CreatureName = input("Enter an animal:")
+Verb1 = input("Enter a verb:")
+PlurN1 = input("Enter a plural noun: ")
+OrgName = input("Enter a proper noun: ")
 
-print("In the", adj1, "town of")
-print(TownName + ", trouble is brewing.")
+
+print("In the", adj1, "town of", TownName, ", trouble is brewing.")
+print("Roaming packs of ", CreatureName, Verb1, " through the streets.")
+print("Luckily for the ", PlurN1, "the ", OrgName, " squad is on the hunt."  )
